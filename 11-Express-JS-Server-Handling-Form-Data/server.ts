10fe752aff0914d48f42c05824d24ff3099990df
eunhyung <ooooo5h91@gmail.com>
2022-11-07 16:00:38 +0900

    Form Data 핸들링

---
+++ 11-Express-JS-Server-Handling-Form-Data/server.ts
@@ -0,0 +1,22 @@
+import express, { response } from "express";
+import userRouter from "./router/userRouter";
+
+const app: express.Application = express();
+
+const hostname: string = "127.0.0.1";
+const port: number = 5000;
+
+// configure express to receive form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).json({ message: "welcome express" });
+});
+
+// router configuration
+app.use("/users", userRouter);
+
+app.listen(port, hostname, () => {
+  console.log("express server is started!!!");
+});
